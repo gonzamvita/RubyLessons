eea52687772364e18b83aae3bb8dcd17ff79fef3
gonzamvita <gonzamv@gmail.com>
2015-10-26 10:33:49 +0100

    Primeras explicaciones

---
+++ palindromo.rb
@@ -0,0 +1,7 @@
+def is_palindrome? (palindrom_candidate)
+  clean_palindrome = palindrom_candidate.downcase.gsub(" ", "").gsub(",", "")
+
+  return(clean_palindrome.reverse == clean_palindrome)
+end
+
+puts is_palindrome?("Race car")
